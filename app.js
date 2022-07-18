169a658072c7611ed880d22d88cfb115cb474c3b
Krishna Sai Biradar <krishnasaibalaji@gmail.com>
2022-07-17 23:05:38 -0400

    update html

---
+++ app.js
@@ -0,0 +1,22 @@
+const inputs = document.querySelectorAll(".input-angle")
+const clickbutton = document.querySelector(".click-button")
+
+const output1 = document.querySelector("#output")
+
+function calculatesum(angle1, angle2, angle3) {
+    const sumangle = angle1 + angle2 + angle3
+    return sumangle
+}
+
+function istriangle() {
+    const sumofangles = calculatesum(Number(inputs[0].value), Number(inputs[1].value), Number(inputs[2].value))
+    if (sumofangles === 180) {
+        output1.innerText = "Yes it form a triangle";
+    } else {
+        output1.innerText= "No it is not a traingle";
+    }
+}
+
+
+
+clickbutton.addEventListener("click", istriangle)
